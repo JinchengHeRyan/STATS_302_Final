352ec1aecb385b011fbaf3ae036690f320d25b3c
JinchengHeRyan <delteforce@outlook.com>
2020-05-04 23:27:01 +0800

    Update mtcnn

--- Age_Rcg/Age_recog_v2_mtcnn.py
+++ Age_Rcg/Age_recog_v2_mtcnn.py
@@ -4,14 +4,14 @@ sys.path.append('../')
 import cv2
 import os
 from Model.SSR_net import SSR_net
-from Age_Rcg.funcs.assist_func import draw_faces, face_count
+from Age_Rcg.funcs.assist_func import draw_faces, face_count, mtcnn_detect
 import time
 
-model = SSR_net(image_size=200, stage_num=[3, 3, 3], lambda_local=0.25, lambda_d=0.25)()
-model.load_weights('../Output/output_3/weights-improvement-24-8.21.h5')
+# model = SSR_net(image_size=200, stage_num=[3, 3, 3], lambda_local=0.25, lambda_d=0.25)()
+# model.load_weights('../Output/output_3/weights-improvement-24-8.21.h5')
 
 
-face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface_improved.xml')
+# face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface_improved.xml')
 
 
 def realtime_recog():
@@ -22,7 +22,6 @@ def realtime_recog():
         pass
 
     img_size = (200, 200)
-    stage_num = [3, 3, 3]
     cap = cv2.VideoCapture(0)
 
     # detected = ''  # make this not local variable
@@ -37,19 +36,21 @@ def realtime_recog():
 
         if img_idx == 1 or img_idx % skip_frame == 0:
             # detect faces using LBP detector
-            gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-            detected = face_cascade.detectMultiScale(gray_img, 1.1)
+            # gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+            # detected = face_cascade.detectMultiScale(gray_img, 1.1)
+
+            detected, ages_output = mtcnn_detect(input_img, img_size)
 
             print("Detected {} faces! ".format(face_count(detected)) if face_count(detected) > 0 else "Detect No Faces! ")
 
-            input_img, ages_output = draw_faces(detected=detected, input_img=input_img, ad=ad, img_size=img_size, model=model)
+            # input_img, ages_output = draw_faces(detected=detected, input_img=input_img, ad=ad, img_size=img_size, model=model)
 
-            if face_count(detected)> 0:
+            if face_count(detected) > 0:
                 print('\t' + ages_output + 'years old')
 
             cv2.imwrite(img_out_path + '/' + str(img_idx) + '.png', input_img)
-            cv2.imshow('result', input_img)
-            cv2.waitKey(1)
+            # cv2.imshow('result', input_img)
+            # cv2.waitKey(1)
 
 
 def static_recog(input_img_path: str):
@@ -58,14 +59,17 @@ def static_recog(input_img_path: str):
         os.mkdir(img_out_path)
     except OSError:
         pass
-    input_img = cv2.imread(input_img_path)
-    gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-    detected = face_cascade.detectMultiScale(gray_img, 1.1)
-    print("Detected {} faces! ".format(face_count(detected)) if face_count(detected) > 0 else "Detect No Faces! ")
 
     ad = 0.25
     img_size = (200, 200)
-    input_img, ages_output = draw_faces(detected=detected, input_img=input_img, ad=ad, img_size=img_size, model=model)
+
+    input_img = cv2.imread(input_img_path)
+
+    detected, ages_output = mtcnn_detect(input_img, img_size)
+    print("Detected {} faces! ".format(face_count(detected)) if face_count(detected) > 0 else "Detect No Faces! ")
+
+
+    # input_img, ages_output = draw_faces(detected=detected, input_img=input_img, ad=ad, img_size=img_size, model=model)
 
     if face_count(detected) > 0:
         print('\t' + ages_output + 'years old')
@@ -74,10 +78,10 @@ def static_recog(input_img_path: str):
 
 
 if __name__ == '__main__':
-    Mode = 1        # 0 is real time mode, 1 is static mode
+    Mode = 0        # 0 is real time mode, 1 is static mode
 
     if Mode == 0:
         realtime_recog()
     else:
-        input_file_path = '2.jpeg'
+        input_file_path = '6.jpg'
         static_recog(input_file_path)
--- Age_Rcg/funcs/assist_func.py
+++ Age_Rcg/funcs/assist_func.py
@@ -1,6 +1,8 @@
 import cv2
 import keras
 import numpy as np
+from Model.mtcnn.mtcnn_model import mtcnn
+from Model.SSR_net import SSR_net
 
 
 def face_count(detected):
@@ -32,8 +34,8 @@ def draw_faces(detected, input_img, ad, img_size, model: keras.Model):
         yw2 = min(int(y_2 + ad * h), img_h - 1)
 
         faces = np.zeros(shape=(len(detected), img_size[0], img_size[1], 3))
-        faces[i, :, :, :] = cv2.resize(input_img[yw1: yw2 + 1, xw1: xw2 + 1, :], img_size) / 255
-        # faces[i, :, :, :] = cv2.resize(input_img[y_1: y_2 + 1, x_1: x_2 + 1, :], img_size) / 255
+        # faces[i, :, :, :] = cv2.resize(input_img[yw1: yw2 + 1, xw1: xw2 + 1, :], img_size) / 255
+        faces[i, :, :, :] = cv2.resize(input_img[y_1: y_2 + 1, x_1: x_2 + 1, :], img_size) / 255
 
         cv2.rectangle(input_img, (x_1, y_1), (x_2, y_2), (255, 0, 0), 2)
         cv2.rectangle(input_img, (xw1, yw1), (xw2, yw2), (0, 0, 255), 2)
@@ -58,3 +60,59 @@ def draw_faces(detected, input_img, ad, img_size, model: keras.Model):
     # cv2.imshow('result', input_img)
 
     return input_img, ages_output
+
+
+def mtcnn_detect(img, img_size):
+    model = mtcnn()
+    threshold = [0.5, 0.6, 0.7]
+    rectangles = model.detectFace(img, threshold)
+
+    faces = []
+    detected = []
+    for i in range(len(rectangles)):
+        rectangle = rectangles[i]
+        print('rectangle = ', rectangle)
+        if rectangle is not None:
+            W = -int(rectangle[0]) + int(rectangle[2])
+            H = -int(rectangle[1]) + int(rectangle[3])
+            paddingH = 0.01 * W
+            paddingW = 0.02 * H
+            crop_img = img[int(rectangle[1] + paddingH):int(rectangle[3] - paddingH),
+                       int(rectangle[0] - paddingW):int(rectangle[2] + paddingW)]
+            if crop_img is None:
+                continue
+            if crop_img.shape[0] < 0 or crop_img.shape[1] < 0:
+                continue
+
+            x_1 = int(rectangle[0])
+            y_1 = int(rectangle[1])
+            x_2 = int(rectangle[2])
+            y_2 = int(rectangle[3])
+
+            detected.append((x_1, y_1, x_2, y_2))
+
+            faces.append(cv2.resize(img[y_1: y_2 + 1, x_1: x_2 + 1, :], img_size) / 255)
+
+            cv2.rectangle(img, (int(rectangle[0]), int(rectangle[1])), (int(rectangle[2]), int(rectangle[3])),
+                          (255, 0, 0), 2)
+
+            for i in range(5, 15, 2):
+                cv2.circle(img, (int(rectangle[i + 0]), int(rectangle[i + 1])), 2, (0, 255, 0))
+
+    faces = np.array(faces)
+
+    if len(detected) > 0:
+        model_2 = SSR_net(image_size=200, stage_num=[3, 3, 3], lambda_local=0.25, lambda_d=0.25)()
+        model_2.load_weights('../Output/output_3/weights-improvement-24-8.21.h5')
+        ages_pred = model_2.predict(faces)
+        ages_output = str()
+        for i in range(len(ages_pred)):
+            ages_output += str(int(ages_pred[i])) + ' '
+    else:
+        ages_output = None
+
+    for i, (x_1, y_1, x_2, y_2) in enumerate(detected):
+        age_label = str(int(ages_pred[i]))
+        draw_label(input_img=img, loc=(x_1, y_1), label=age_label)
+
+    return detected, ages_output
